ccaaa4fcdedf1b8d93c5829570a17168fe8e7587
unknown <adrian.fra97@gmail.com>
2023-02-26 18:12:01 +0100

    added tests module with test data and tests for data_loader module

---
+++ cars_manager_app/tests/utils.py
@@ -0,0 +1,7 @@
+from decimal import Decimal
+
+from cars_manager_app.cars.model import Car
+
+bmw = Car("BMW", Decimal('160000.0'), "BLACK", 1500, ["ABS", "ALLOY WHEELS"])
+mazda = Car("MAZDA", Decimal('120000.0'), "WHITE", 2500, ["AIR CONDITIONING", "BLUETOOTH"])
+fiat = Car("FIAT", Decimal('140000.0'), "WHITE", 2500, ["AIR CONDITIONING", "BLUETOOTH"])
