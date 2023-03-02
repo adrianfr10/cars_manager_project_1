85733cb7c705f06d29f375184fd386137d738e09
unknown <adrian.fra97@gmail.com>
2023-03-02 19:27:34 +0100

    added enums module in cars package and sort method in service module without test

---
+++ cars_manager_app/cars/enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Sort(Enum):
+    COLOR = 1
+    MILEAGE = 2
+    MODEL = 3
+    PRICE = 4
--- cars_manager_app/cars/service.py
+++ cars_manager_app/cars/service.py
@@ -1,11 +1,27 @@
 from .model import Car
-from itertools import chain
-from decimal import Decimal
-from collections import defaultdict, Counter
-from statistics import mean
+from .enums import Sort
 
 
 class CarsService:
 
     def __init__(self, cars: list[Car]) -> None:
         self.cars = cars
+
+    # -----------------------------------------------------------------------------------------------
+    def sort(self, sort: Sort, descending: bool) -> list[Car]:
+        """
+        Method sorts a collection of Car objects, color, mileage, price or model wise, according to
+        an argument given, and has also a possibility of sorting in ascending or descending order.
+        :param sort:
+        :param descending:
+        :return:
+        """
+        match sort:
+            case Sort.COLOR:
+                return sorted(self.cars, key=lambda car: car.color, reverse=descending)
+            case Sort.MILEAGE:
+                return sorted(self.cars, key=lambda car: car.mileage, reverse=descending)
+            case Sort.MODEL:
+                return sorted(self.cars, key=lambda car: car.model, reverse=descending)
+            case Sort.PRICE:
+                return sorted(self.cars, key=lambda car: car.price, reverse=descending)
