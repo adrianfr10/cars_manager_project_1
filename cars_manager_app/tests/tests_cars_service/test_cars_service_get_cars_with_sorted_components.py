4e53c2dab019af186917438ba0002cbd334f2272
unknown <adrian.fra97@gmail.com>
2023-05-17 22:47:47 +0200

    added parametrization for test_cars_service_get_cars_with_sorted_components

--- cars_manager_app/tests/tests_cars_service/test_cars_service_get_cars_with_sorted_components.py
+++ cars_manager_app/tests/tests_cars_service/test_cars_service_get_cars_with_sorted_components.py
@@ -1,11 +1,16 @@
+import pytest
+
 from ..utils import *
 
-#todo do poprawy, moze się da parametrize napewno sie da
-def test_when_list_of_cars_with_sorted_components_is_correct(cars_service) -> None:
-    cars_with_sorted_components = cars_service.get_cars_with_sorted_components()
-    sorted_bmw, sorted_mazda, sorted_fiat = sorted(bmw.components), sorted(mazda.components), sorted(fiat.components)
-    assert cars_with_sorted_components[0].components == sorted_bmw
-    assert cars_with_sorted_components[1].components == sorted_mazda
-    assert cars_with_sorted_components[2].components == sorted_fiat
 
+@pytest.mark.parametrize(
+    "car_index, expected_sorted",
+    [
+        (0, sorted(bmw.components)),
+        (1, sorted(mazda.components)),
+        (2, sorted(fiat.components)),
+    ])
+def test_when_list_of_cars_with_sorted_components_is_correct(cars_service, car_index, expected_sorted) -> None:
+    cars_with_sorted_components = cars_service.get_cars_with_sorted_components()
+    assert cars_with_sorted_components[car_index].components == expected_sorted
 
