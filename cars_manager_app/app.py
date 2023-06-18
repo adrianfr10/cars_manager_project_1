ec01f26333ee2d07aaece466a2e811cffb7cc390
unknown <adrian.fra97@gmail.com>
2023-06-18 19:48:16 +0200

    added cars_service in main() for use in routes

--- cars_manager_app/app.py
+++ cars_manager_app/app.py
@@ -1,9 +1,18 @@
+from cars_manager_app.data_loader.factory.car.factory.processor.data_processor import DataProcessor
+from cars_manager_app.data_loader.factory.car.factory.json_car_factory import JsonCarFactory
+from cars_manager_app.cars.service import CarsService
+import os
 
 
-def main() -> None:
+def main():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_path, '..', 'data', 'cars_test.json')
+    cars = DataProcessor(JsonCarFactory()).process(file_path)
+    cars_service = CarsService(cars)
 
     # Place here in future code to manage cars service methods
-    pass
+
+    return cars_service
 
 
 if __name__ == '__main__':
