58cc59f70ad7f3b6297644d00a7e6270f66db3dc
unknown <adrian.fra97@gmail.com>
2023-11-30 23:19:43 +0100

    small changes in test package and converter module

--- cars_manager_app/data_loader/factory/car/converter/to_car_converter.py
+++ cars_manager_app/data_loader/factory/car/converter/to_car_converter.py
@@ -7,5 +7,5 @@ class ToCarConverter(Converter):
 
     def convert(self, data: list[dict[str, Any]]) -> list[Any]:
         if not data:
-            raise ValueError("Invalid data")
+            raise ValueError("Invalid data_test")
         return [Car.of(car_data) for car_data in data]
--- cars_manager_app/tests/tests_cars_service/cars_service_utils.py
+++ cars_manager_app/tests/tests_cars_service/cars_service_utils.py
@@ -7,6 +7,6 @@ from cars_manager_app.cars.model import Car
 
 
 def init_cars(file_name: str) -> list[Car]:
-    FILE_BASE_PATH: Final = '\\'.join([os.path.dirname(os.path.dirname(__file__)), 'data\\'])
+    FILE_BASE_PATH: Final = '\\'.join([os.path.dirname(os.path.dirname(__file__)), 'data_test\\'])
     filename = f'{FILE_BASE_PATH}{file_name}'
     return DataProcessor(JsonCarFactory()).process(filename)
